cdfdee903530b4590644fe91739eea1674e067ea
David Galicia <galiciadavid13@gmail.com>
2023-04-18 16:06:51 -0500

    update: added Google Maps

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function myMap() {
+    var mapProp = {
+      center: new google.maps.LatLng(41.87552, -87.630717),
+      zoom: 10,
+    };
+    var map = new google.maps.Map(
+      document.getElementById("googleMap"),
+      mapProp
+    );
+  }
